e5e14b0d278b07c932e78603c1518b036941cbe6
TechALynch <Anthony.Lynch001@outlook.com>
2023-09-26 18:55:13 -0400

    D2-DOM1_Update

--- script.js
+++ script.js
@@ -1,39 +1,76 @@
-// const apiKey = '4Rw0XFcv6C27LQjtS8fc7KqoH4ZlqKPlgGczMbv5kR1Uhx8UWExlICIZ';
-
-
 console.log("working")
 
-//Global variables
-const dicUrlApi = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-const result = document.getElementById("result")
-const sound = document.getElementById("sound")
-const btn = document.getElementById("search-btn")
+// Global variables
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
 
-btn.addEventListener("click", () => {
-    let inpWord = document.getElementById("inp-word").value; //Gets the value (char) of the input
-    console.log(inpWord)
+btn.addEventListener("click", async () => {
+    let dicUrlApi = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+    let inpWord = document.getElementById("inp-word").value.toLowerCase(); // Gets the value (char) of the input
+    console.log(inpWord);
 
-//     //Needs Annotation
-//     fetch(`${dicUrlApi}${inpWord}`)
-//         .then((response) => response.json)
-//         .then((data) => console.log(data));
+    try {
+        // Make the API request
+        let response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${inpWord}`);
+        console.log(response)
+
+        // Drilling our data response and setting DOM
+        //Return Word
+        let WordNameReturn = response.data[0].word;
+        let wordName = document.getElementById("wordName");
+        wordName.innerHTML = `${WordNameReturn}`;
+
+        // Get Part of speech(s)
+        let posReturn = response.data[0].meanings[0].partOfSpeech;
+        let pos = document.getElementById("pos");
+        pos.innerHTML = `${posReturn}`;
+
+        // Get phonetic(s)
+       let phoneticReturn = response.data[0].phonetic;
+       let phonetic = document.getElementById("phonetic");
+        phonetic.innerHTML = `${phoneticReturn}`;
+
+        // Get Definition(s)
+       let defReturn = response.data[0].meanings[0].definitions[0].definition;
+       let def = document.getElementById("def");
+       def.innerHTML = `${defReturn}`;
+
+       //Get Word Example(s)
+       let wordExReturn = response.data[0].meanings[0].definitions[0].example;
+       let ex = document.getElementById("word-example");
+       ex.innerHTML = `${wordExReturn}`;
+        ////Iteration
+        // for (let i = 0; i < response.data.definitions.length; i++) {
+        // let def = response.data[i].meanings[0]
+        // let defUpdate = document.getElementsByClassName("word-meaning")
+        // defUpdate.textContent = def
+        // defUpdate.appendChild(defUpdate)
+        // }
+
+        // Error message
+    } catch (error) {
+        // Show an error message in a popup
+        alert(`Error: Word was not found in API/database! Please re-enter a valid term`);
+    }
 });
 
-const image = document.getElementById("searchImage");
-const wordExamples = document.getElementById("word-example");
-const toggleButton = document.getElementById("toggleButton");
+const image = document.getElementById("searchImage")
+const wordExamples = document.getElementById("word-example")
+const toggleButton = document.getElementById("toggleButton")
 let isImageVisible = true;
 
 toggleButton.addEventListener("click", function () {
-    if (isImageVisible) {
-        wordExamples.style.display = "block"; // display
-        image.style.display = "block";
-        toggleButton.textContent = "View Less";
-    } else {
-        wordExamples.style.display = "none"; // hide
-        image.style.display = "none";
-        toggleButton.textContent = "View More";
+    switch (isImageVisible) {
+        case true:
+            wordExamples.style.display = "none"; // hide
+            image.style.display = "none";
+            toggleButton.textContent = "View More";
+            break;
+        default:
+            wordExamples.style.display = "block"; // display
+            image.style.display = "block";
+            toggleButton.textContent = "View Less";
     }
-    isImageVisible = !isImageVisible;
+    isImageVisible = !isImageVisible
 });
-
